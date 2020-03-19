ec929217227c823cb9460cc2852c9b51b9240649
Kunal Gupta <kunall.gupta17@gmail.com>
2020-03-19 10:57:23 +0530

    working caching

--- app/src/main/java/com/kunall17/trellassignment/ADapter.java
+++ app/src/main/java/com/kunall17/trellassignment/ADapter.java
@@ -3,6 +3,7 @@ package com.kunall17.trellassignment;
 import android.content.Context;
 import android.net.Uri;
 import android.util.Log;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
@@ -11,10 +12,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.exoplayer2.DefaultRenderersFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.upstream.DataSpec;
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.upstream.cache.Cache;
 import com.google.android.exoplayer2.upstream.cache.CacheDataSourceFactory;
+import com.google.android.exoplayer2.upstream.cache.CacheUtil;
 import com.google.android.exoplayer2.upstream.cache.LeastRecentlyUsedCacheEvictor;
 import com.google.android.exoplayer2.upstream.cache.SimpleCache;
 
@@ -22,14 +25,18 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ADapter extends RecyclerView.Adapter<Viddd> {
 
+    public static final long DEFAULT_MAX_CACHE_FILE_SIZE = 5 * 1024 * 1024;
     private final CacheDataSourceFactory dataSourceFactory;
     private final SimpleExoPlayer player;
+    private final DefaultHttpDataSourceFactory factory;
+    private final Context context;
     int lastINdex = -1;
     private ArrayList<String> a = new ArrayList<>();
-    private Cache cache;
+    private SimpleCache cache;
 
     public ADapter(Context context) {
         a.add("https://cdn.trell.co/h_640,w_640/user-videos/videos/orig/P7sls2VpRAJW8Vbz6rnUlU6WvLTZwEhp.mp4");
@@ -43,16 +50,94 @@ public class ADapter extends RecyclerView.Adapter<Viddd> {
         a.add("https://cdn.trell.co/h_640,w_640/user-videos/videos/orig/r7EsSAGF6a1q2vXdZXFDUCTJ7wMLBGEO.mp4");
         a.add("https://cdn.trell.co/h_640,w_640/user-videos/videos/orig/fDkn4hLtkApjqyVq6vEItYKUcr8Kgxlf.mp4");
 
+        this.context = context;
         File file = new File(context.getCacheDir(), "media");
         cache = new SimpleCache(file, new LeastRecentlyUsedCacheEvictor(1048576000));
         DefaultRenderersFactory defaultRenderersFactory = new DefaultRenderersFactory(context).setExtensionRendererMode(DefaultRenderersFactory.EXTENSION_RENDERER_MODE_PREFER);
-        DefaultHttpDataSourceFactory factory = new DefaultHttpDataSourceFactory("android-marsplay-video-player");
+        factory = new DefaultHttpDataSourceFactory("android-marsplay-video-player");
         dataSourceFactory = new CacheDataSourceFactory(getSimpleCache(), factory);
         player = new SimpleExoPlayer.Builder(context, defaultRenderersFactory).build();
 
-        ExtractorMediaSource mediaSource = new ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(Uri.parse(a.get(0)));
+        ProgressiveMediaSource mediaSource = new ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(Uri.parse(a.get(0)));
         player.prepare(mediaSource);
         player.seekTo(0);
+    }
+
+    private void preCache(int position, Context context) {
+
+        String url = a.get(position);
+        DataSpec dataSpec = new DataSpec(Uri.parse(url), 0, 1024 * 1024, null);
+        Pair<Long, Long> cached = CacheUtil.getCached(dataSpec, cache, null);
+        Log.d("SEEHERE", "preCache: " + cached);
+
+        if (cached.second != 0) {
+            Log.d("SEEHERE", "!!!!preCache: " + cached);
+            return;
+        }
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    CacheUtil.cache(dataSpec, cache, null, dataSourceFactory.createDataSource(), new CacheUtil.ProgressListener() {
+                        @Override
+                        public void onProgress(long requestLength, long bytesCached, long newBytesCached) {
+                            Log.d("ADapterseehere", position + "onProgress() called with: requestLength = [" + requestLength + "], bytesCached = [" + bytesCached + "], newBytesCached = [" + newBytesCached + "]");
+                        }
+                    }, new AtomicBoolean(false));
+                    System.out.println("seehere");
+                } catch (Exception e) {
+                    System.out.println("Exception");
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+
+//
+//        AsyncTask.execute(() -> {
+//            Log.d("SEEHERE", "preCache: executing");
+//            PriorityTaskManager mPriorityTaskManager = new PriorityTaskManager();
+//            mPriorityTaskManager.add(C.PRIORITY_DOWNLOAD);
+//
+//            DataSource cacheReadDataSource = new FileDataSource();
+//            DefaultDataSourceFactory upstreamFactory =
+//                    new DefaultDataSourceFactory(context, factory);
+//            DataSink cacheWriteDataSink = new CacheDataSink(cache, DEFAULT_MAX_CACHE_FILE_SIZE);
+//            DataSource upstream = upstreamFactory.createDataSource();
+//            upstream = new PriorityDataSource(upstream, mPriorityTaskManager, C.PRIORITY_DOWNLOAD);
+//
+//            int progressive_download = Log.e("progressive_download", "Create priority upstream data source!");
+//            CacheDataSource cacheDataSource = new CacheDataSource(cache, upstream, cacheReadDataSource,
+//                    cacheWriteDataSink, 0, new CacheDataSource.EventListener() {
+//                @Override
+//                public void onCachedBytesRead(long cacheSizeBytes, long cachedBytesRead) {
+//                    Log.d("SEEHERE", "onCachedBytesRead() called with: cacheSizeBytes = [" + cacheSizeBytes + "], cachedBytesRead = [" + cachedBytesRead + "]");
+//                }
+//
+//                @Override
+//                public void onCacheIgnored(int reason) {
+//                    Log.d("SEEHERE", "onCacheIgnored() called with: reason = [" + reason + "]");
+//                }
+//            });
+//
+//            AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+//            try {
+//                Log.d("SEEHERE", "preCache: ");
+//                CacheUtil.cache(dataSpec, cache, null, cacheDataSource, new CacheUtil.ProgressListener() {
+//                    @Override
+//                    public void onProgress(long requestLength, long bytesCached, long newBytesCached) {
+//                        Log.d("SEEHERE", "onProgress() called with: requestLength = [" + requestLength + "], bytesCached = [" + bytesCached + "], newBytesCached = [" + newBytesCached + "]");
+//                    }
+//                }, atomicBoolean);
+//                Log.d("SEEHERE", "preCache: done ");
+//                cacheDataSource.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        });
     }
 
     @NonNull
@@ -65,7 +150,9 @@ public class ADapter extends RecyclerView.Adapter<Viddd> {
     @Override
     public void onBindViewHolder(@NonNull Viddd holder, int position) {
         Log.d("ADapterseehere", "onBindViewHolder() called with: holder = [" + holder + "], position = [" + position + "]");
-
+        if (getItemCount() > position + 1) {
+            preCache(position + 1, context);
+        }
     }
 
     private Cache getSimpleCache() {
@@ -94,11 +181,12 @@ public class ADapter extends RecyclerView.Adapter<Viddd> {
     public void setPlayer(int s, @NotNull Viddd holder) {
         Log.d("ADapterseehere", "setPlayer() called with: s = [" + s + "], holder = [" + holder + "]");
         if (lastINdex != s) {
-            ExtractorMediaSource mediaSource = new ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(Uri.parse(a.get(s)));
+            ProgressiveMediaSource mediaSource = new ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(Uri.parse(a.get(s)));
             player.prepare(mediaSource);
             player.seekTo(0);
             lastINdex = s;
             holder.player.setPlayer(player);
+            player.setPlayWhenReady(true);
         }
     }
 }
